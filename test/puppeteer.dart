fc5b7f059ba8fcee02af6835200d7ef1f3fe33a7
Leif Linse <leif.linse@gmail.com>
2020-09-23 01:54:42 +0200

    Make puppeteer test a test

--- test/puppeteer.dart
+++ test/puppeteer.dart
@@ -1,29 +1,34 @@
 import 'dart:io';
 
+import 'package:flutter_test/flutter_test.dart';
 import 'package:puppeteer/puppeteer.dart';
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_static/shelf_static.dart';
 
-void main() async {
-  // Serve the web build
-  var server = await io.serve(createStaticHandler('build/web'), 'localhost', 0);
+void main() {
+  test('puppetteer', () async {
+    // Serve the web build
+    var server =
+        await io.serve(createStaticHandler('build/web'), 'localhost', 0);
 
-  // Open in chrome
-  var browser = await puppeteer.launch(
-    defaultViewport: DeviceViewport(
-      width: 200,
-      height: 200,
-    ),
-  );
-  var page = await browser.newPage();
-  await page.goto('http://localhost:${server.port}/index.html',
-      wait: Until.domContentLoaded);
+    // Open in chrome
+    var browser = await puppeteer.launch(
+      defaultViewport: DeviceViewport(
+        width: 200,
+        height: 200,
+      ),
+    );
+    var page = await browser.newPage();
+    await page.goto('http://localhost:${server.port}/index.html',
+        wait: Until.domContentLoaded);
 
-  // Is there a better way to wait for MyHomePage to render?
-  await Future.delayed(Duration(seconds: 1));
+    // Is there a better way to wait for MyHomePage to render?
+    await Future.delayed(Duration(seconds: 1));
 
-  // Save screenshot
-  var screenshot = await page.screenshot();
-  await File('chrome.png').writeAsBytes(screenshot);
-  await browser.close();
+    // Save screenshot
+    var screenshot = await page.screenshot();
+    expect(screenshot.length, greaterThan(0));
+    await File('chrome.png').writeAsBytes(screenshot);
+    await browser.close();
+  });
 }
